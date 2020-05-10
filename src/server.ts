6350d78bae26887831e26e2ed663b209da1ff29c
Matheus Neves <msneves@protonmail.com>
2020-05-10 17:15:38 -0300

    Configurando o ESLint

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3333;
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World'});
-});
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(port, () => {
   console.log(`🚀 Server started on port ${port}`);
